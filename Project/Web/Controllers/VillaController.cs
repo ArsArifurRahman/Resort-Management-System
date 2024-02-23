6299cf6684c1410c6c8a7d3e6ef3d3579caf6922
ArsArifurRahman <ars.ict.developer@gmail.com>
2024-02-23 20:21:46 +0600

    Villa Model

---
+++ Project/Web/Controllers/VillaController.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Persistence.Data;
+
+namespace Web.Controllers;
+public class VillaController : Controller
+{
+    private readonly DataContext _context;
+
+    public VillaController(DataContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_context.Villas.ToList());
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Villa villa)
+    {
+        if (villa.Name == villa.Description)
+        {
+            ModelState.AddModelError("name", "The description cannot exactly match the Name.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _context.Villas.Add(villa);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Villa");
+        }
+        return View();
+    }
+}
